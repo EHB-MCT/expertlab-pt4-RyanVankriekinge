2ffb6e8a570a4957bfbb0f42e26c9566f851cc89
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-11-07 10:11:47 +0100

    feat: Adding navigation with router links to App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,17 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper">
-
-      <nav>
-
-      </nav>
+    <div class="outer-wrapper">
+      <div class="wrapper">
+        <nav>
+          <ul>
+            <li><RouterLink to="/">Home</RouterLink></li>
+            <li><RouterLink to="/about">More About Me</RouterLink></li>
+            <li><RouterLink to="/works">My Works</RouterLink></li>
+            <li><RouterLink to="/contact">Contact</RouterLink></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 
