cff5dd307f85d626c2b7c839e5d77d6fbe3d7fd4
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-11-07 11:11:56 +0100

    feat: animating hamburger menu

--- src/components/HamburgerMenu.vue
+++ src/components/HamburgerMenu.vue
@@ -66,6 +66,53 @@
       margin-top: -1px;
       transition: transform .6s cubic-bezier(.165, .84, .44, 1), opacity .3s cubic-bezier(.165, .84, .44, 1), background-color .6s cubic-bezier(.165, .84, .44, 1);
   }
+
+  .burger-bar--1 {
+      -webkit-transform: translateY(-6px);
+      transform: translateY(-6px);
+      top: 40%;
+  }
+
+  .burger-bar--2 {
+      transform-origin: 100% 50%;
+      transform: scaleX(1);
+  }
+
+  .burger-button:hover .burger-bar--2 {
+      transform: scaleX(1);
+  }
+
+  .no-touchevents .burger-bar--2:hover {
+      transform: scaleX(1);
+  }
+
+  .burger-bar--3 {
+      transform: translateY(6px);
+      top: 60%;
+  }
+
+  #burger.active .burger-button {
+      transform: rotate(-180deg);
+  }
+
+  #burger.active .burger-bar {
+      background-color: lighten($primary, 10);
+  }
+
+  #burger.active .burger-bar--1 {
+      transform: rotate(45deg);
+      top: 50%;
+  }
+
+  #burger.active .burger-bar--2 {
+      opacity: 0;
+  }
+
+  #burger.active .burger-bar--3 {
+      transform: rotate(-45deg);
+      top: 50%;
+  }
+
   @media screen and (max-width: 640px) {
       #burger {
           display: block;
