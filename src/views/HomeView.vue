ff2c9f14d24bd97ec71faca0853f3bdbfb94e7d6
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-11-12 15:34:55 +0100

    feat: shortening double animation into one method in timeline

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@ onMounted(() => {
     duration: 1.5,
     ease: 'power3.out',
   })
-    .to({}, { duration: 0.5 })
+    .to({}, {})
     .from('.welcome-text-name', {
       x: '20vw',
       opacity: 0,
@@ -18,25 +18,13 @@ onMounted(() => {
       ease: 'power3.out',
     })
     .to({}, { duration: 0.5 })
-    .from(
-      '.function-svg',
-      {
-        x: '-100vw',
-        duration: 1.5,
-        ease: 'power3.out',
-      },
-      '-=1.5',
-    )
-    .from(
-      '.jobtitle',
-      {
-        x: '-20vw',
-        opacity: 0,
-        duration: 1.5,
-        ease: 'power3.out',
-      },
-      '-=1.5',
-    )
+    .from(['.function-svg', '.jobtitle'], {
+      x: '-100vw',
+      opacity: 0,
+      duration: 1.5,
+      ease: 'power3.out',
+      stagger: 0,
+    })
 })
 </script>
 
@@ -66,12 +54,12 @@ onMounted(() => {
                 class="function-svg"
                 viewBox="0 0 100 100"
                 xmlns="http://www.w3.org/2000/svg"
-                width="100vw"
+                width="100%"
                 height="auto"
               >
                 <polygon points="-500,0 -500,35 85,35 70,0" fill="#57378B" />
               </svg>
-              <p class="jobtitle">Multimedia developer</p>
+              <p class="jobtitle">Portfolio</p>
             </div>
             <div class="welcome-text-skills"></div>
           </div>
