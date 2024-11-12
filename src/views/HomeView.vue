d0e03feb33b0b55c0325eaf0b755c41d78831a92
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-11-12 14:42:19 +0100

    feat: welcome section responsief maken

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,18 +5,35 @@
     <div class="outer-wrapper">
       <div class="wrapper">
         <div class="section-welcome">
-          <div class="welcome-text-name">
-            <p class="title welcome-ryan">Ryan</p>
-            <p class="title welcome-vankriekinge">Vankriekinge</p>
+          <div class="welcome-text-namegroup">
+            <div class="welcome-text-name">
+              <p class="title welcome-ryan">Ryan</p>
+              <p class="title welcome-vankriekinge">Vankriekinge</p>
+            </div>
             <svg
               class="name-svg"
               viewBox="0 0 100 100"
               xmlns="http://www.w3.org/2000/svg"
-              width="100%"
-              height="100%"
+              width="100vw"
+              height="auto"
             >
-              <polygon points="50,10 15,75 500,75 500,10" fill="#57378B" />
+              <polygon points="50,10 0,75 500,75 500,10" fill="#57378B" />
             </svg>
+          </div>
+          <div class="welcome-text-career">
+            <div class="welcome-text-jobtitle">
+              <svg
+                class="function-svg"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100vw"
+                height="auto"
+              >
+                <polygon points="-500,0 -500,35 85,35 70,0" fill="#57378B" />
+              </svg>
+              <p class="jobtitle">Multimedia developer</p>
+            </div>
+            <div class="welcome-text-skills"></div>
           </div>
         </div>
       </div>
