48f6ce47521206fea40930ba328fa475c7ca1cb1
RyanVankriekinge <123389635+RyanVankriekinge@users.noreply.github.com>
2024-11-21 09:21:16 +0100

    feat: adjusting repo paths to deploy page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'expertlab-pt3-RyanVankriekinge',
+  base: 'expertlab-pt4-RyanVankriekinge',
   plugins: [vue()],
   resolve: {
     alias: {
